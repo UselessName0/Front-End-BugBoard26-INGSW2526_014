8fc343f2b950ec5b7187c66fc408f0195b5b30c8
GiuGiuh <Pippobarretta1309@gmail.com>
2025-11-28 18:00:21 +0100

    commit di utenteservice

---
+++ src/main/java/org/example/bugboard26frontend/APIServices/UtenteService.java
@@ -0,0 +1,12 @@
+package org.example.bugboard26frontend.APIServices;
+
+import org.example.bugboard26frontend.Entita.Utente;
+
+public class UtenteService {
+
+    private final ApiClient api = ApiClient.getApiClient();
+
+    public Utente getUserById(Long userId) {
+        return null;
+    }
+}
